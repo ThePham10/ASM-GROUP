8cbb809a528ffa649a0bb4a8f8860aa09f1fce31
ThePham10 <s3981133@rmit.edu.vn>
2024-08-25 14:08:16 +0700

    add mebers pages and styles

---
+++ full-stack-project-main/frontend/src/pages/MembersPage.jsx
@@ -0,0 +1,29 @@
+import NavBar from '../components/NavBar';
+import GroupHeaderBox from '../components/GroupHeaderBox';
+import { modelgroups } from "../model/GroupModel";
+import MembersBox from '../components/MembersBox.jsx';
+
+const MembersPage = ({ groupID }) => {
+    const group = modelgroups.find((group) => group.id === groupID);
+
+    if (!group) {
+        return <div>Group not found</div>;
+    }
+
+  return (
+    <div style={{ background: '#B9D9DC' }}>
+      <NavBar />
+      <GroupHeaderBox groupID={groupID} /> {/* Assigning the id prop to GroupHeaderBox */}
+      <div className="grid grid-cols-12 gap-4 p-4">
+        <div className="col-span-3"></div>
+        <div className="col-span-6">
+          <div className="mb-4"></div>
+            <MembersBox groupID={groupID} />
+        </div>
+        <div className="col-span-3"> </div>
+      </div>
+    </div>
+  );
+};
+
+export default MembersPage;
